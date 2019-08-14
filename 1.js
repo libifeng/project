fdf052769cdaccd302409afc847833718dcd6813
new Vue <1192431450@qq.com>
2019-08-14 21:21:01 +0800

    init

---
+++ 1.js
@@ -0,0 +1,25 @@
+//找到divpages的第一个元素，绑定单击事件
+divpages.firstElementChild.onclick=function(){
+
+}
+//找到divpages的最后一个元素，绑定单击事件
+divpages.lastElementChild.onclick=function(){
+
+}
+
+
+
+
+
+//查找table下thead下的input
+var chvAll=docunment.querySelector("table>thead input");
+
+//绑定单击事件：
+chbAll.onclick=function(){
+
+}
+
+//遍历所有input
+for(var chb  of  chbs){
+
+}
